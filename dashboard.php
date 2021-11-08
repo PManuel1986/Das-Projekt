df3c099f5d3c899f1c2545c034aed90205704e40
Manuel Pinnecke <91166974+PManuel1986@users.noreply.github.com>
2021-11-08 02:28:19 +0100

    Projekt Dashboard
    Dashboard ohne Bootstrap und Javascript Mit Puren HTML und CSS

---
+++ dashboard.php
@@ -0,0 +1,63 @@
+<body>
+       
+        
+        
+        <nav>
+
+<div class="fixed-navbar-default">
+  <div class="logoSpace">
+    <h1 class="admin"> Dashboard</h1>    
+    <div class="navbar-group">ADMIN</div>
+    <a class="btn-home" href="#"><span class="glyphicon glyphicon-tag"></span> Startseite</a>
+     <a class="btn-home" href="#"><span class="glyphicon glyphicon-tag"></span> Aktualisierungen</a>
+    <a class="btn-home" href="#"><span class="glyphicon glyphicon-tag"></span> Seiten</a>
+    <a class="btn-home" href="#"><span class="glyphicon glyphicon-tag"></span> Benutzer</a>
+  </div>
+  
+  <div class="logoSpace">
+    <div class="navbar-group">REDAKTION</div>
+    
+    <a class="btn-home" href="#"><span class="glyphicon glyphicon-tag"></span> Medien</a>
+    <a class="btn-home" href="#"><span class="glyphicon glyphicon-tag"></span> Beiträge</a>
+     
+  </div>
+  
+  <div class="container-buttons">
+    <div class="logoSpace">
+      <div class="navbar-group">RADIO</div>
+      <a class="btn-home" href="#"><span class="glyphicon glyphicon-tag"></span> Sendeplan</a>
+      
+    </div>
+  </div>
+</div>
+</nav>
+   
+
+  <input type="checkbox" name="trigger" id="trigger">
+  <label for="trigger"></label>
+
+<div class="site-wrap">
+  
+<div  class="section group"class="site-wrap">
+	<div class="col span_1_of_3">
+	<center>
+      <h1> Statistics </h1>
+       </center>
+	</div>
+	<div class="col span_1_of_3">
+	<center>
+      <h1 >Neue Bewerbung </h1>
+       </center>
+	</div>
+	<div class="col span_1_of_3">
+	<center>
+      <h1 >Neuer Wunsch / Gruß </h1>
+       </center>
+	</div>
+
+</div>
+ 
+   <div class="footer">
+  <p>Footer</p>
+</div>
+</div>
